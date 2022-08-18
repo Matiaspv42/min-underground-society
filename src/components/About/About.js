194da54323a45cc2b4bbf766abaca5534f421583
Matias <mpintov42@gmail.com>
2022-08-17 21:35:39 -0400

    styling on about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,7 @@ export default function About () {
     return (
         <div className="about grow">
             <div className='bg-white px-5 py-5 rounded-lg"'>
-                <h1 className='header'>About the team</h1>
+                <h1 className='header'>Meet the team</h1>
                 <div className="cards flex flex-wrap px-5 py-5 ">
                     {cardData.map((d)=>
                         <div className='basis-1/3 flex justify-center'>
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,12 +1,13 @@
+import './card.css'
 export default function Card(props){
     const {data} = props
     return(
         <div className="card w-80 my-7">
             <img src="/img/talon.png" alt="" className="rounded-lg"/>
-            <div className="textCard">
-                <h3>{data.name}</h3>
-                <h4>{data.title} <span className="twitterHandler">{data.twitterHandler}</span></h4>
-                <p>{data.description}</p> 
+            <div className="textCard my-2">
+                <h3 className='text-lg'>{data.name}</h3>
+                <h4 className='text-sm grayColor'><span className='yellowButton'>{data.twitterHandler}</span> <span className="twitterHandler ">{data.title}</span></h4>
+                <p className='text-base my-2 roboto'>{data.description}</p> 
             </div>
         </div>
     )
